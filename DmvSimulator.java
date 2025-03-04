2725243f1014302b4e1701f3a2b96e67551ef9b2
Zakaria Merzougui <zakariamer123@gmail.com>
2025-03-04 17:03:31 -0500

    setup master

---
+++ DmvSimulator.java
@@ -0,0 +1,9 @@
+import java.security.SecureRandom;
+
+public class DmvSimulator {
+    
+    public static void main(String[] args) {
+        System.out.println("Hi, welcome to the DMV");
+    }
+
+}
